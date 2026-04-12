78979e16e0b5865e5a487d00ae51a91ae944a18f
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2026-04-12 16:17:30 +0200

    k(s)'s de prueba

--- Code/Runge Kutta/rk4hillsdef.py
+++ Code/Runge Kutta/rk4hillsdef.py
@@ -10,4 +10,13 @@ ds = 1 # milímetros
 s_end = 1000 # milímetrss 
 s_vector = np.linspace(0,s_end, 1001)
 
-#
+# Para distintas func k(s=) creamos los vectores evaluados en s (?s_Vector')
+kvector1 = s_vector # Es el caso k(s) = s
+kvector2 = np.cos(s_vector) # Es j(S)= cos(s)
+kvector3 = np.sqrt(3)* np.cos(np.sqrt(2)* s_vector) #k(s)= r(3)*cos(r(2)*s)
+
+# Resolviendo hills con rk4
+
+def solvehills(y0, k_vector, ds,s_end):
+
+    return y 
