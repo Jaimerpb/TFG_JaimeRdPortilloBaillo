22988cafb6280091d61da9e2b03212d6d9d2b156
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2026-04-13 14:51:33 +0200

    1ª extensión NO Lineal de Hills, quads + offmomentum + sextupolos: 𝑥^′′+[𝑘_𝑞𝑢𝑎𝑑 (𝑠)(1−𝛿)]𝑥+1/2 𝑘_𝑠𝑒𝑥𝑡 (𝑠) 〖(1−𝛿)𝑥〗^2=0

--- Code/Runge Kutta/HILLS/hills.py
+++ Code/Runge Kutta/HILLS/hills.py
@@ -3,15 +3,15 @@ import numpy as np
 # 1) ONLY QUADS X''+KsX = 0
 
 class onlyquads:
-    def __init__(self,k_func):
-        self.k_func = k_func
+    def __init__(self,kquad_func):
+        self.kquad_func = kquad_func
     
     # esto es la f(s,y) que pasamos al Rk para el ca'lculo de las pendientes
     def __call__(self, s, y):
         x, v = y
         
         #la x''
-        Vprima = -self.k_func(s) * x
+        Vprima = -self.kquad_func(s) * x
         return np.array([v, Vprima])
     
 
@@ -22,24 +22,24 @@ class onlyquads:
 
 # class quads_bending:
     
-#     def __init__(self, k_func,rho_func):
-#         self.k_func = k_func
+#     def __init__(self, kquad_func,rho_func):
+#         self.kquad_func = kquad_func
 #         self.rho_func = rho_func
     
 
 #     def __call__(self, s, y):
 #         x,v = y
 
-#         rhoinv = 1 / (self.rho_func(s) **2)
-#         Vprima = -(rhoinv + self.k_func) *x
+#         kdip = 1 / (self.rho_func(s) **2)
+#         Vprima = -(rhoinv + self.kquad_func) *x
         
 #         return np.array([v, Vprima]) 
 
 # 3) QUADS + BENDING MAGNET + OFF MOMETNUM (no homogneo)
 class hills_off():
-    def __init__(self,k_func , rho_func, delta):
+    def __init__(self,kquad_func , rho_func, delta):
         
-        self.k_func = k_func 
+        self.k_func = kquad_func 
         self.rho_func = rho_func
         
         self.delta= delta 
@@ -47,12 +47,34 @@ class hills_off():
     def __call__(self, s, y):
         x,v = y 
 
-        rhoinv = 1 / (self.rho_func(s) **2)
+        kdip = 1 / (self.rho_func(s) **2)
         inhomterm = self.delta / self.rho_func(s)
 
-        Vprima= -(rhoinv + self.k_func) *x + inhomterm
+        Vprima= -(k_dip + self.kquad_func) *x + inhomterm
         return np.array( [v, Vprima])
 
+## EXTENSIONES NO LINEALES 
+#4 ) QUADS + OFF MOMENTUM + SEXTUPOLES (Hogeneous)
+
+class sext_off():
+    def __init__(self, kquad_func, rho_func, delta, ksext_func):
+        self.kquad_func = kquad_func 
+        self.rho_func = rho_func 
+        self.delta = delta 
+        self.ksext_func = ksext_func 
+
+    def __call__(self, s, y):
+      x,v =  y 
+    
+
+
+      Vprima = -(kquad_func * (1-delta))* x - 1/2*(ksext_func * (1 - delta))*x**2
+
+      return np.array([v, Vprima])
+    
+# 5) QUADS + BEND + OFF MOMMENTUM + SEXTUPOLES 
+
+class
 
 
 
