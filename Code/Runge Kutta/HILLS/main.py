e000ffa46be63ea026c0d81d3b33127068a76be9
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2026-05-26 11:28:46 +0200

    índices de poinca´re

--- Code/Runge Kutta/HILLS/main.py
+++ Code/Runge Kutta/HILLS/main.py
@@ -41,6 +41,15 @@ y0 = np.array([x0,dxds_0]) # Estado inicial del sistema, con pos y ángulo inici
 
 
 
+# Parámetros
+
+#Cond. iniciales
+x0 = 1e-3 # pos incial Xo
+dxds_0 = 0 #aangulo inicial X'o
+y0 = np.array([x0,dxds_0]) # Estado inicial del sistema, con pos y ángulo iniciales
+
+
+
 
 
 def periodic_s(li, lf , f, s):
@@ -61,20 +70,20 @@ def periodic_s(li, lf , f, s):
 
 #Slide 19
 
-k_val = 0.05     # Fuerza del gradiente magnético (1/m2)
+k_val = 0.519     # Fuerza del gradiente magnético (1/m2)
 
-lq1 = 0.2  # Longitud del cuadrupolo (m) 
+lq1 = 0.4  # Longitud del cuadrupolo (m) 
 l_i = 0  # Pos. entrada (m)
 Lc =  1 # Longitud/periodo de la celda (m) 
 
 # Slide 20
  
-ld = 1.5 # Longitud del dipolo (m)
+ld = .015 # Longitud del dipolo (m)
 rho = 3.81 # Radio de curvatura de las partículas en la sección de los dipolos (m)
 Lc2 = 5.8 # Longitud de la celda slide20 (m)
 
 ds = 1e-3 # metros
-s_end= 100*Lc2 # metros
+s_end= 100*Lc # metros
 s_vector = np.arange(0,s_end+ds, ds)
 
 
@@ -112,6 +121,71 @@ modelo1 = hills_lineal_hom(k_func = kquad)
     
 # solucions rk de los modelos
 y1 = rk4(modelo1, y0, s_vector, ds)
+print(y1)
+
+#Estabilidad Numérica
+
+h_step_values = ds/np.array([0.01, 0.1, 0.5, 1 ,2, 4])
+desv_relativa = []
+
+h_anterior = 2* ds
+s_vector_anterior = np.arange(0, s_end+ h_anterior, h_anterior)
+
+y_anterior = rk4(modelo1,y0,s_vector_anterior,h_anterior)
+
+#De la matriz de estados cojo la fila de posiciones
+x_iteracion_anterior = y_anterior[0,:]
+
+
+
+# Comparando respecto al paso anterior
+
+for h in h_step_values:
+    print(f"Solving hills for h = {h} mm")
+    s_vector_actual = np.arange(0, s_end+ h, h)
+
+    y_actual = rk4(modelo1, y0, s_vector_actual, h)
+
+    x_actual = y_actual[0,:]
+
+    #Se interpolan las posiciones sol. de la anterior a las posiciones solición actual
+    # esto es para comparar en los mismos puntos (np,interp)
+    x_anterior_interp = np.interp(s_vector_actual,s_vector_anterior,x_iteracion_anterior)
+
+
+    #Error absoluto yerro relativo
+    #Ahora sí, se están restando vectores de igual dimensión. 
+    error_abs = np.linalg.norm(x_actual - x_anterior_interp)
+    desv_rel = error_abs/ np.linalg.norm(x_anterior_interp)
+
+
+    desv_relativa.append(desv_rel)
+
+    # Ahora 'actualizamos' para la próxima iteración, Xanterior pasa a ser Xactual
+    x_iteracion_anterior = x_actual
+
+    s_vector_anterior = s_vector_actual
+
+print(desv_relativa)
+
+plt.figure(figsize=(10, 6))
+plt.plot(h_step_values, desv_relativa, marker='o', color='purple',  
+linewidth=2.5, markersize=8, label='Desviación relativa')
+
+plt.gca().invert_xaxis() #invierte el ejex para que la gráfica avanze a medida qye h decrec3
+
+
+plt.yscale('log', base= 10)
+
+plt.title("Convergencia RK4: Estabilidad Numérica", fontsize=14, fontweight='bold')
+plt.xlabel("Tamaño del paso h (mm)", fontsize=12)
+plt.ylabel("Desviación relativa", fontsize=12)
+plt.grid(True, which="both", ls="--", alpha=0.5)
+plt.legend(fontsize=11)
+plt.tight_layout()
+plt.show()
+
+
 
 
 #Grafica de k(S)
@@ -130,7 +204,7 @@ plt.show()
 
 
 
-# # Diagrama de fases
+# Diagrama de fases
 # plt.figure(figsize=(10, 6))
 # plt.plot(y1[0,:], y1[1, :]) # representamos pos vs vel
 # plt.title("Diagrama de Fases de Hills")
@@ -140,13 +214,41 @@ plt.show()
 # plt.show()    
 
 
-# plt.figure(figsize=(10, 4))
-# plt.plot(s_vector, y1[0, :], label='Posición (x)')
-# plt.plot(s_vector, y1[1, :], label='Velocidad (v)')
-# plt.title("Hills eq usando RK4")
-# plt.xlabel("Tiempo (t)")
-# plt.ylabel("Amplitud")
-# plt.legend()
-# plt.grid(True)
-# plt.show()
+plt.figure(figsize=(10, 4))
+plt.plot(s_vector, y1[0, :], label='Posición (x)')
+plt.plot(s_vector, y1[1, :], label='Velocidad (v)')
+plt.title("Hills eq usando RK4")
+plt.xlabel("Tiempo (t)")
+plt.ylabel("Amplitud")
+plt.legend()
+plt.grid(True)
+plt.show()
 
+
+#Mapa de Poincaré (Simulando FODO onlyquads, slide 19, con varios Xo)
+
+
+def INDpoincare(s_vector, period):
+    # tomando la sección fija s = n*Lc, Lc es el perido.
+    #Elegimos el pto de la malla discreta(de s_vector) más cercano a cada múltiplo entero del periodod.
+    s_targets = np.arange(s_vector[0], s_vector[-1]+ 0.5*period, period) #estos son los 'ptos teóricos',se generan los pts s= n*Lx
+    
+    #searchsorted para buscar eos ptos en la malla
+    indx = np.searchsorted(s_vector, s_targets) 
+    # indx = np.clip(indx, 1,len(s_vector)- 1)
+    
+    #s e comparan el nodo izq y el der para elegi rel más cercano
+    ileft = indx-1 
+    iright = indx # ileft y right son dos nodos contiguos alrededor del talget
+
+    # para ver si el nodod derecho está más cerca que le izq
+    cond = np.abs(s_vector[iright]-s_targets)< np.abs(s_targets - s_vector[ileft])
+    #np.where(condition, [x, y, ] /) returns elements chosen from x or y depending on a condition
+    closest = np.where(cond, iright,ileft )
+
+    return np.unique(closest) #devuelve índice únicos, evita repetdos por redondeo
+
+
+# Ahora, función que guarda una sol. en un .txt
+
+def 
