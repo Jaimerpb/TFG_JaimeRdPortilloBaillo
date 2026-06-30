b510ee324a8fcce5dca1d4d2c46a8b9749bb55f9
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2026-06-30 22:52:32 +0200

    plots

--- Code/MAIN/HILLS/numericalstability.py
+++ Code/MAIN/HILLS/numericalstability.py
@@ -54,7 +54,7 @@ def numstability(modeloEDO, y0, s_end, ds, filename):
     plt.figure(figsize=(10, 6))
     plt.plot(h_step_values, desv_relativa, marker='o', color='purple',  
              linewidth=2.5, markersize=8, label='Desviación relativa')
-    plt.axhline(y=1e-6, color='red', linestyle='--', linewidth=2, label=r'Tolerancia ($10^{-6}$)')
+    
     plt.gca().invert_xaxis() # invierte el eje x para que la gráfica avance a medida que h decrece
     plt.xscale('log', base= 10)    
     plt.yscale('log', base=10)  
@@ -62,7 +62,7 @@ def numstability(modeloEDO, y0, s_end, ds, filename):
     
 
     plt.title("Convergencia RK4: Estabilidad Numérica", fontsize=14, fontweight='bold')
-    plt.xlabel("Tamaño del paso h (m)", fontsize=12)
+    plt.xlabel("Tamaño del paso ds (m)", fontsize=12)
     plt.ylabel("Desviación relativa", fontsize=12)
     plt.grid(True, which="both", ls="--", alpha=0.5)
     plt.legend(fontsize=11)
